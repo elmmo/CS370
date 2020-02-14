0a35b1d302d146322d731e0110b4a89cf816238e
ELIZABETH MIN <elmm682@gmail.com>
2020-02-13 21:08:58 -0800

    add recursive dependency tokens

--- scanner_parser/src/scanner_parser/AdhocScanner.java
+++ scanner_parser/src/scanner_parser/AdhocScanner.java
@@ -10,8 +10,9 @@ import java.util.regex.Pattern;
 public class AdhocScanner {
 	private Scanner input; 
 	private static final String FILE_NAME = "CalcInput1.txt";
-	private String[] expNames = {"whitespace"};
-	private String[] exp = {"[\\s]"};
+	private static final int NEG_CONSTANT = -100; // token dependency length not to exceed this
+	private String[] expNames = {"whitespace", "standardops", "assign_2", "assign_1"};
+	private String[] exp = {"[\\s]", "\\(|\\)|\\+|\\-|\\*", ":", "="};
 	private HashMap<String,Pattern> regex;
 	private ArrayList<String> tokens;
 	
@@ -42,27 +43,86 @@ public class AdhocScanner {
 		}
 	}
 	
+	/* called by the scan function to go through character by character and find tokens
+	 * @param	line	the string to search for tokens
+	 */
+	private void scanForTokens(String line) {
+		String[] characters = line.split("");
+		try {
+			for (int i = 0; i < characters.length; i++) {
+				boolean unassigned = true; 
+				int j = 1; 
+				
+				while (j < expNames.length && unassigned) {
+					Pattern p = regex.get(expNames[j]);
+					if (p.matcher(characters[i]).matches()) {
+						String[] name = expNames[j].split("_"); 
+						if (name.length > 1) {
+							int tokenNum = verifyDependentTokens(characters, i, j);
+							i += tokenNum; 
+							if (tokenNum > 0) { 
+								tokens.add(name[0]);
+							}
+						}
+						unassigned = false; 
+					} else if (j+1 >= expNames.length) {
+						System.out.println("Problem");
+						throw new Exception("Lexical Error. Please try again.");
+					}
+					j++; 
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/** 
+	 * verifies that the necessary characters follow a starting character for a given token 
+	 * @param characters	characters inputed by the user 
+	 * @param charIndex		the index to start searching from in the characters
+	 * @param tokenIndex	the index to start searching from in the tokens 
+	 * @return				an int indicating how many tokens match the pattern 
+	 * @throws Exception	throws an error in the case that there are no more characters to match 
+	 */
+	private int verifyDependentTokens(String[] characters, int charIndex, int tokenIndex) throws Exception { 
+		if (charIndex + 1 < characters.length) { 
+			String[] name = expNames[tokenIndex+1].split("_");
+			Pattern p = regex.get(expNames[tokenIndex+1]);
+			if (p.matcher(characters[charIndex+1]).matches()) {
+				return (Integer.parseInt(name[1]) == 1) ? 1 : verifyDependentTokens(characters, charIndex+1, tokenIndex+1) + 1;
+			}
+		
+		}
+		return NEG_CONSTANT; 
+	}
+	
 	
 	/* will prompt the user for string to scan
 	 * @param prompt	the string to prompt the user for input
 	 */
 	public String getInput(String prompt) {
-		System.out.print(prompt);
+		//System.out.print(prompt);
 		String res = input.next();
 		// clears the buffer for the next input
 		input.nextLine();
 		return res;
 	}
 	
-	public void scan() {
+	// scans through the input file and returns the tokens picked up by the scanner
+	public ArrayList<String> scan() {
 		String line = "";
 		while (input.hasNextLine()) {
 			// takes in input from next file and removes all whitespace
 			line = input.nextLine();
 			line = line.replace("\n", "");
 			Pattern p = regex.get("whitespace");
-			System.out.println(p.matcher(line).replaceAll("")); 
+			
+			// scans through the line char by char
+			scanForTokens(line);
 		}
+		System.out.println(tokens);
+		return tokens;
 	}
 	
 	/*
